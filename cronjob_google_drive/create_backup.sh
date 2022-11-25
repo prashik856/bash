bac274945052009963b17efd339dc7f7ab3c0048
Prashik <prashik.raut@oracle.com>
2022-11-25 19:27:54 +0530

    Updated script

--- cronjob_google_drive/create_backup.sh
+++ cronjob_google_drive/create_backup.sh
@@ -15,33 +15,36 @@ log () {
   printf '%s ' "$(date -u)"; echo "${message}";
 }
 
+log "Getting script working directory";
+cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
+log "Script Working Directory: ${cwd}";
+
+
+configDirectory="${cwd}/config.json";
 log "Reading inputs from config file";
 
-secretsDirectory=$(jq -r '.secretsDirectory' config.json);
+secretsDirectory=$(jq -r '.secretsDirectory' ${configDirectory});
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 log "Secrets Directory: ${secretsDirectory}";
 
-credentials=$(jq -r '.password' config.json);
+credentials=$(jq -r '.password' ${configDirectory});
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 
 decryptedPass=$(echo ${credentials} | base64 --decode);
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 log "Zip file password: ${decryptedPass}";
 
-innerZipFileName=$(jq -r '.innerZipFileName' config.json);
+innerZipFileName=$(jq -r '.innerZipFileName' ${configDirectory});
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 log "Inner Zip file name: ${innerZipFileName}";
 
-zipFileName=$(jq -r '.zipFileName' config.json);
+zipFileName=$(jq -r '.zipFileName' ${configDirectory});
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 log "zip file name: ${zipFileName}";
 
-log "Done";
-
-log "Deleting already present zip files";
-rm -f ${innerZipFileName};
-rm -f ${zipFileName};
-log "Done";
+syncDirectory=$(jq -r '.syncDirectory' ${configDirectory});
+commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
+log "Sync Directory: ${syncDirectory}";
 
 log "Creating inner zip file";
 zip ${innerZipFileName} -r ${secretsDirectory};
@@ -53,7 +56,10 @@ zip --password ${decryptedPass} ${zipFileName} ${innerZipFileName};
 commandExitCode=$?; checkCommandExitCode "${message}" ${commandExitCode};
 log "Done";
 
-# Zip file creation completed
 log "Deleting inner zip file";
 rm -f ${innerZipFileName};
+log "Done";
+
+log "Moving zip file to sync directory";
+mv -f ${zipFileName} ${syncDirectory}/${zipFileName};
 log "Done";
