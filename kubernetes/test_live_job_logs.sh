5e1174887fc1d7b462b6cf228ce55e0b0c99e4a4
Prashik <prashik.raut@oracle.com>
2023-07-24 19:38:14 +0530

    Updated live job logs script

--- kubernetes/test_live_job_logs.sh
+++ kubernetes/test_live_job_logs.sh
@@ -26,10 +26,35 @@ echo "Creating job";
 kubectl -n "${namespace}" apply -f "${jobFile}";
 
 echo "Printing job logs";
-kubectl -n "${namespace}" logs -f job.batch/"${jobName}";
-# We cannot get logs any longer. Either pod job succeeded or it got restarted.
+count=0;
+while [[ ${count} -lt 20 ]];
+do
+  # This command always exits with 0 regardless if job failed or passed.
+  # If we are not able to get job logs, that means that job did not start.
+  kubectl -n "${namespace}" logs -f job.batch/"${jobName}";
+  exitCode=$?;
+  if [[ ${exitCode} -ne 0 ]];
+  then
+    count=$((count+1));
+    log "Trying again to get logs again: ${count}..."
+    sleep 6;
+  else
+    break;
+  fi;
+done;
 
+if [[ ${count} -ge 20 ]];
+then
+  kubectl -n "${namespace}" get job "${jobName}";
+  log "Count not start job. Exiting...";
+  exit 255;
+fi;
+
+sleep 10;
+
+# We cannot get logs any longer. Either pod job succeeded or it got restarted.
 echo "Getting status of the running job";
+kubectl -n "${namespace}" get job "${jobName}";
 status=$(kubectl -n "${namespace}" get job "${jobName}" -o jsonpath={.status.succeeded} --ignore-not-found=true)
 echo "Job Status: ${status}";
 
